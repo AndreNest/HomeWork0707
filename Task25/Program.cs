e7eea420bb17a64e7383db0bdff1107a9b5f4dc4
AndreyNesterov <1@nestandre.ru>
2022-07-07 00:50:50 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача № 25. Программа принимает два числа а и б. После чего возводит число А в степень Б
+
+int exponen(int a, int b)
+{
+    int sum = (int)Math.Pow(a, b);
+    Console.WriteLine("Число А в степени Б = " +sum);
+
+    return sum;
+}
+exponen(Convert.ToInt32(Console.ReadLine()),
+        Convert.ToInt32(Console.ReadLine()));
