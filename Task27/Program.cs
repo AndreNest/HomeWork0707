a1781f1f6be045764e919255b91f68e58fb1ee7a
AndreyNesterov <1@nestandre.ru>
2022-07-07 02:51:03 +0300

    add Task27/29

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 27. Програама принимает число. И выдает сумму цифр.
+Console.Write("Введите число X: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while(n!=0)
+{
+    s = s + n % 10;
+    n = n / 10;
+}
+if(n == 0)
+{
+    Console.WriteLine("Нечего складывать!");
+}
+Console.WriteLine("---------------------------");
+Console.WriteLine("Сумма чисел равна: {0}",s);
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите число N: ");
+int [] array(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        int newNumb = (new Random()).Next(0,99);
+        result[i] = newNumb;
+        Console.Write(result[i] + " ");
+    }
+
+    return result;
+
+}
+
+array(Convert.ToInt32(Console.ReadLine()));
